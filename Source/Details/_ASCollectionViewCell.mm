510b6a35777bcbe9c9207815659d797cec1b7dcb
Kael <kael.zs17@gmail.com>
2020-01-08 16:58:11 +0800

    fix: collection cell background color and selected background view

--- Source/Details/_ASCollectionViewCell.mm
+++ Source/Details/_ASCollectionViewCell.mm
@@ -25,6 +25,16 @@
 {
   ASDisplayNodeAssertMainThread();
   ASCellNode *node = element.node;
+
+  if (node) {
+    self.layer.cornerRadius = node.cornerRadius;
+    self.backgroundColor = node.cellBackgroundColor;
+    self.selectedBackgroundView = node.selectedBackgroundView;
+    self.backgroundView = node.backgroundView;
+
+    self.clipsToBounds = node.clipsToBounds;
+  }
+
   node.layoutAttributes = _layoutAttributes;
   _element = element;
   
